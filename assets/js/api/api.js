375873d1b77ccbac976f900c36f943299a65b8bb
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-01 11:46:55 -0300

    feat: creation and implementation of pokemon cards

--- assets/js/api/api.js
+++ assets/js/api/api.js
@@ -1,17 +1,9 @@
-let paginacao = {
-  offset: 0,
-  limit: 100,
-  botao_voltar: 0,
-  botao_avancar: 2,
-};
-
-export async function buscaAPI(offset, limit) {
+export async function FindAllApi(offset, limit) {
   const resp = await fetch(
     `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
   );
 
   const data = await resp.json();
-  console.log(data);
 
   return data;
 }
--- assets/js/scripts/index.js
+++ assets/js/scripts/index.js
@@ -1,3 +1,35 @@
-import { buscaAPI } from "../api/api.js";
+import { FindAllApi } from "../api/api.js";
 
-buscaAPI(0, 3);
+async function Machine() {
+  const sectionCards = document.getElementById("card-presentation");
+  console.log(sectionCards);
+  const pokemonsFindAll = await FindAllApi(0, 12);
+
+  pokemonsFindAll.results.map(async (item) => {
+    const pokemonFetch = await fetch(item.url);
+    const pokemon = await pokemonFetch.json();
+    console.log(pokemon);
+
+    const name = pokemon.name;
+    let img = pokemon.sprites.other["official-artwork"].front_default;
+    if (!img) {
+      img = "./assets/img/pokemon_sem_img.png";
+    }
+
+    const cards = `
+    <div class="card-pokemon">
+      <figure>
+        <img
+          class="card-pokemon--img"
+          src=${img}
+        />
+        </figure>
+        <p class="card-pokemon--name">${name}</p>
+    </div>
+    `;
+
+    sectionCards.insertAdjacentHTML("beforeend", cards);
+  });
+}
+
+Machine();
